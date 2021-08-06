59324e5aab139b976abea711944f51e3742fc8c1
Shorockhov-A <andr2409@bk.ru>
2021-08-06 13:48:42 +0300

    task_3_5 modify

--- lesson_3/task_3_5.py
+++ lesson_3/task_3_5.py
@@ -1,10 +1,18 @@
-numbers = (-5, -18, -10, 0, 5, 12, -3, -11, -19, -7, -3)
+numbers = (5, -5, -18, -10, 0, 5, 12, -3, -11, -19, -7, -3)
 print(numbers)
-max_negative_el = numbers[0]
+negative_els = []
 for item in numbers:
-    if max_negative_el < item < 0:
-        max_negative_el = item
+    if item < 0:
+        negative_els.append(item)
 
-max_el_idx = [idx for idx in range(len(numbers)) if numbers[idx] == max_negative_el]
-print(f'Максимальный отрицательный элемент в массиве: {max_negative_el}\n'
-      f'Позиция элементов в массиве: {", ".join(map(str, max_el_idx))}')
+if negative_els:
+    max_negative_el = negative_els[0]
+    for item in negative_els:
+        if item > max_negative_el:
+            max_negative_el = item
+
+    max_el_idx = [idx for idx in range(len(numbers)) if numbers[idx] == max_negative_el]
+    print(f'Максимальный отрицательный элемент в массиве: {max_negative_el}\n'
+          f'Позиция элементов в массиве: {", ".join(map(str, max_el_idx))}')
+else:
+    print('В массиве отсутствуют отрицательные элементы.')
